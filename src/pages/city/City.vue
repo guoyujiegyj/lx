0c43619eb606ef5bdc271279894a8c98a8a7b2bd
gyj <1401357358@qq.com>
2019-08-31 19:57:22 +0800

    城市也面的布局完成。better-scroll已经下载。下一步使用better-scroll进行页竖向面滚动

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,6 +2,8 @@
   <div>
     <Header></Header>
     <Search></Search>
+    <List></List>
+    
   </div>
 </template>
 
@@ -9,12 +11,14 @@
 //引入头部组件。
 import Header from './components/header'
 import Search from './components/Search'
+import List from './components/List'
 export default {
   name: 'City',
   //注册组件
    components:{
      Header,
-     Search
+     Search,
+     List
    }
 }
 </script>
---
+++ src/pages/city/components/list.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="list">
+    <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+            <div class="button-wrapper">
+                <div class="button">北京</div>
+               
+            </div>
+            
+        </div>
+    </div>
+    <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+            <div class="button-wrapper">
+                <div class="button">北京</div>
+               
+            </div>
+            <div class="button-wrapper">
+                <div class="button">北京</div>
+               
+            </div>
+            <div class="button-wrapper">
+                <div class="button">北京</div>
+               
+            </div>
+            <div class="button-wrapper">
+                <div class="button">北京</div>
+               
+            </div>
+            <div class="button-wrapper">
+                <div class="button">北京</div>
+               
+            </div>
+        </div>
+    </div>
+    <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class=item-list>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+        </div>
+    </div>
+    <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class=item-list>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+        </div>
+    </div>
+    <div class="area">
+        <div class="title border-topbottom">C</div>
+        <div class=item-list>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+            <div class="item border-bottom">阿拉善</div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/varible.scss';
+    .list{
+        position: absolute;
+        top:1.4rem;
+        left:0;
+        right:0;
+        bottom:0;
+        background:red;
+        overflow: hidden;
+        .border-topbottom{
+            &::before{
+                border-color:#ccc;
+            }
+            
+            &:after{
+                border-color: #777;
+            }
+        }
+        .title{
+            line-height: .4rem;
+            background: #eee;
+            padding-left: .2rem;
+            color:#666;
+            font-size: .26rem;
+        }
+        .button-list{
+            padding:.1rem .6rem .1rem .1rem;
+            overflow: hidden;
+            .button-wrapper{
+                float:left;
+                width:33.33%;
+                .button{
+                    margin:.1rem;
+                    text-align: center;
+                    border:.02rem solid #ccc;
+                    padding:.1rem;
+                    border-radius:.06rem;
+                }
+            }
+            
+        }
+        .item-list{
+                .item{
+                    line-height:.76rem;
+                    color:#666;
+                    padding-left:.2rem;
+                }
+            }
+    }
+</style>
